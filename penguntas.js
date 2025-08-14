bdcbd5420bf009737d1f59ff103626fe9efd93b6
Bicudinho18 <marcus.santos.silva@escola.pr.gov.br>
2025-08-14 15:32:10 -0300

    Add files via upload

---
+++ penguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+  'História',
+  'Em que ano ocorreu a Proclamação da República no Brasil?',
+  '1889.'
+);
+
+criaCartao(
+  'Geografia',
+  'Qual é o maior rio do mundo em volume de água?',
+  'Rio Amazonas.'
+);
+
+criaCartao(
+  'Ciência',
+  'Qual é o elemento químico representado pelo símbolo O?',
+  'Oxigênio.'
+);
+
+criaCartao(
+  'Matemática',
+  'Quanto é 7 x 8?',
+  '56.'
+);
+
+criaCartao(
+  'Literatura',
+  'Quem escreveu "Dom Casmurro"?',
+  'Machado de Assis.'
+);
+
+criaCartao(
+  'Inglês',
+  'Como se diz "livro" em inglês?',
+  'Book.'
+);
+
+criaCartao(
+  'Esportes',
+  'Quantos jogadores um time de futebol tem em campo?',
+  '11 jogadores.'
+);
+
+criaCartao(
+  'Arte',
+  'Quem pintou a Mona Lisa?',
+  'Leonardo da Vinci.'
+);
